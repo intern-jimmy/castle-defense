27592c2a352e856758868405076f17d246bd9a53
Intern Jimmy <bogside.jimmy@gmail.com>
2023-04-24 23:11:54 -0400

    setting up some basic utilities for getting the profile and heroes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Game from '$lib/game';
+  let playerAddress = '';
+
+  async function init() {
+    console.log(playerAddress)
+    const game = await Game.initializeName(playerAddress);
+    
+    // Do something with the initialized game object
+  }
+
+</script>
+
+
+<h1>Welcome to Castle Defender</h1>
+<label>Wallet: </label> <input bind:value={playerAddress}>
+<button on:click={init}>Get Profile</button>
